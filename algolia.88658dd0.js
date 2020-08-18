7ef6672ccd070d5b1e67398a46ae76af92bfd51f
G M Bueno (via Travis CI) <gmbueno1802@gmail.com>
2020-08-18 19:09:10 +0000

    Deploy Leaf-Agriculture/docs to github.com/Leaf-Agriculture/docs.git:gh-pages

--- algolia.88658dd0.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[30],{114:function(n,w,o){}}]);
---
+++ algolia.a85e4a29.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[25],{107:function(n,w,o){}}]);
